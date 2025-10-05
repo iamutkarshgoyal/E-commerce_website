e96d1cf827cac7033733bf100bfbe98fca250192
Utkarsh Goyal <utkarshgoyal@Utkarshs-MacBook-Air.local>
2025-10-05 23:28:30 +0530

    Ignore Photos Library folder

---
+++ src/MyComponents/WomenGymwear.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function WomenGymwear() {
+  return (
+    <div className="container my-5">
+      <h1>WomenGymwear</h1>
+      <p>WomenGymwear.</p>
+    </div>
+  );
+}
